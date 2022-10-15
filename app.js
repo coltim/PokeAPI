7eb4124ef42e361cf1caacb16260d66a0d982902
coltim <cseketomi1100@gmail.com>
2022-10-15 12:07:41 +0200

    set types color

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const button = document.querySelector('#btn');
 
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
+const typeColor = {
+  bug: '#26de81',
+  dragon: '#ffeaa7',
+  electric: '#fed330',
+  fairy: '#FF0069',
+  fighting: '#30336b',
+  fire: '#f0932b',
+  flying: '#81ecec',
+  grass: '#00b894',
+  ground: '#EFB549',
+  ghost: '#a55eea',
+  ice: '#74b9ff',
+  normal: '#95afc0',
+  poison: '#6c5ce7',
+  psychic: '#a29bfe',
+  rock: '#2d3436',
+  water: '#0190FF',
+};
+
 const generateRandomPokemon = () => {
   randomUrl = url + generateRandomNumber();
   fetch(randomUrl)
@@ -25,6 +44,9 @@ const displayPokemon = (data) => {
   const statAttack = data.stats[1].base_stat;
   const statDefense = data.stats[2].base_stat;
   const statSpeed = data.stats[5].base_stat;
+
+  const themeColor = typeColor[data.types[0].type.name];
+
   const html = `
         <p class="hp">
           <span>hp</span>
@@ -52,6 +74,7 @@ const displayPokemon = (data) => {
   card.innerHTML = html;
 
   appendTypes(data.types);
+  setTypeColor(themeColor);
 };
 
 const generateRandomNumber = () => Math.floor(Math.random() * 600 + 1);
@@ -61,6 +84,13 @@ const appendTypes = (types) => {
     let span = document.createElement('span');
     span.textContent = item.type.name;
     document.querySelector('.types').appendChild(span);
+  });
+};
+
+const setTypeColor = (color) => {
+  card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #fff 36%)`;
+  card.querySelectorAll('.types span').forEach((typeColor) => {
+    typeColor.style.backgroundColor = color;
   });
 };
 
